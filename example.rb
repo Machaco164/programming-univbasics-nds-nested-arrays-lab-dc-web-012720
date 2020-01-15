c0b4fb0777d6404be8f53217e25c4e79c3f2c308
Raul A Rozo <raul.a.rozo.o@gmail.com>
2020-01-15 22:34:49 +0000

    Automatically backed up by Learn

--- example.rb
+++ example.rb
@@ -1,3 +1,5 @@
 spice_rack = [["Mace", "Ginger", "Marjoram"],
               ["Paprika", "Fajita Mix", "Coriander"],
               [ "Parsley", "Sage", "Rosemary"]]
+              
+puts spice_rack[1][1]
